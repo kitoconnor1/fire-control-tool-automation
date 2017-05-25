43744e80bb47dcf7264123e3a444cf5e38792315
Quresh Latif <qureshlatif@yahoo.com>
2017-05-25 15:25:28 -0600

    Distance to canyon, flat, steep, and ridge
    - Produces the 4 desired distance input layers for boosted regression tree analysis.
    - Looks for and starts with raw DEM layer downloaded from LANDFIRE web service.
    - Improves cleanup of workspace over previous version.

--- Topography_inputs.py
+++ Topography_inputs.py
@@ -6,38 +6,53 @@ import os, sys
 
 arcpy.env.workspace = r"E:\GISData\fire_control_modl_proj" # Set workspace #
 arcpy.env.overwriteOutput = True
-DEM = "DEM.tif"
+SA = u'Tonto_study_area.shp' # Polygon defining study landscape #
+
+# Mask downloaded DEM to study area #
+DEM = ExtractByMask(r'US_DEM2010/US_DEM2010.tif', SA)
+DEM.save("dem.tif")
+DEM = Raster('dem.tif')
 
 if not os.path.isdir("BRTinputs"):
     os.makedirs("BRTinputs")
 
-arcpy.gp.Slope_sa("DEM.tif", "BRTinputs/slope.tif", "PERCENT_RISE", "1") # slope input
-arcpy.gp.Aspect_sa("DEM.tif", "BRTinputs/aspect.tif") # topographic aspect
+arcpy.gp.Slope_sa(DEM, "BRTinputs/slope.tif", "PERCENT_RISE", "1") # slope input
+arcpy.gp.Aspect_sa(DEM, "BRTinputs/aspect.tif") # topographic aspect
 
 ###################### Topographic position input ####################################
-arcpy.gp.FocalStatistics_sa("DEM.tif", "DEM_200m.tif", "Circle 200 MAP", "MEAN", "NODATA") # 200m neighborhood DEM mean
-tpi = Raster("DEM.tif") - Raster("DEM_200m.tif")
-tpi.save("tpi.tif")
-arcpy.gp.Slope_sa("DEM.tif", "slope_deg.tif", "DEGREE", "1") # slope input
-steep = Raster("slope_deg.tif") > 6
-steep.save("steep.tif")
-tpi_1 = (Raster("tpi.tif") < (-12))
-tpi_1.save("tpi_1.tif")
-tpi_2 = ((Raster("tpi.tif") <= 12) & (Raster("tpi.tif") >= (-12))) * (1 - Raster("steep.tif")) * 2
-tpi_2.save("tpi_2.tif")
-tpi_3 = ((Raster("tpi.tif") <= 12) & (Raster("tpi.tif") >= (-12))) * Raster("steep.tif") * 3
-tpi_3.save("tpi_3.tif")
-tpi_4 = (Raster("tpi.tif") > 12)*4
-tpi_4.save("tpi_4.tif")
-tpi_class = Raster("tpi_1.tif") + Raster("tpi_2.tif") + Raster("tpi_3.tif") + Raster("tpi_4.tif")
-tpi_class.save("BRTinputs/tpi_class.tif")
+DEM_200 = arcpy.gp.FocalStatistics_sa("DEM.tif", "DEM_200m.tif", "Circle 200 MAP", "MEAN", "NODATA") # 200m neighborhood DEM mean
+tpi = DEM - DEM_200
+slope = arcpy.gp.Slope_sa(DEM, "slope_deg.tif", "DEGREE", "1") # slope input
+steep = Raster(slope) > 6
+tpi_1 = tpi < (-12)
+tpi_1 = Reclassify(tpi_1, "Value", RemapValue([[0, "NODATA"], [1, 1]]))
+tpi_dist = EucDistance(tpi_1, cell_size = 30)
+tpi_dist = ExtractByMask(tpi_dist, SA)
+tpi_dist.save("BRTinputs/Dist_canyon.tif")
+del(tpi_1, tpi_dist)
 
-arcpy.Delete_management("steep.tif")
+tpi_1 = ((tpi <= 12) & (tpi >= (-12))) * (1 - steep)
+tpi_1 = Reclassify(tpi_1, "Value", RemapValue([[0, "NODATA"], [1, 1]]))
+tpi_dist = EucDistance(tpi_1, cell_size = 30)
+tpi_dist = ExtractByMask(tpi_dist, SA)
+tpi_dist.save("BRTinputs/Dist_flat.tif")
+del(tpi_1, tpi_dist)
+
+tpi_1 = ((tpi <= 12) & (tpi >= (-12))) * steep
+tpi_1 = Reclassify(tpi_1, "Value", RemapValue([[0, "NODATA"], [1, 1]]))
+tpi_dist = EucDistance(tpi_1, cell_size = 30)
+tpi_dist = ExtractByMask(tpi_dist, SA)
+tpi_dist.save("BRTinputs/Dist_steep.tif")
+del(tpi_1, tpi_dist)
+
+tpi_1 = tpi > 12
+tpi_1 = Reclassify(tpi_1, "Value", RemapValue([[0, "NODATA"], [1, 1]]))
+tpi_dist = EucDistance(tpi_1, cell_size = 30)
+tpi_dist = ExtractByMask(tpi_dist, SA)
+tpi_dist.save("BRTinputs/Dist_ridge.tif")
+del(tpi_1, tpi_dist)
+
 arcpy.Delete_management("slope_deg.tif")
-arcpy.Delete_management("tpi.tif")
-arcpy.Delete_management("tpi_1.tif")
-arcpy.Delete_management("tpi_2.tif")
-arcpy.Delete_management("tpi_3.tif")
-arcpy.Delete_management("tpi_4.tif")
 arcpy.Delete_management("DEM_200m.tif")
+del(steep, slope, DEM_200)
 ######################################################################################
