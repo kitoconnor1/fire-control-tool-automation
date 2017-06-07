ae4217bd204b59f6f15233fe87f69ecf3bac4f97
Quresh Latif <qureshlatif@yahoo.com>
2017-06-07 10:17:16 -0600

    adds annotation, removes roads
    Adds a title specifying objective. Removes extraction of roads layer because roads are a subset of streets.

--- Retrieve_roads_&_streets.py
+++ Retrieve_roads_&_streets.py
@@ -1,3 +1,5 @@
+##### Clips street layer from NAVTEQ source file for use in various inputs #####
+
 import arcpy
 from arcpy import env
 from arcpy.sa import *
@@ -7,9 +9,6 @@ import os, sys
 arcpy.env.workspace = r"E:\GISData\fire_control_modl_proj" # Set workspace #
 SA = "Tonto_study_area.shp" # Polygon defining study landscape #
 source = "T:\\FS\\RD\\RMRS\\Science\\HD\\MySpace\\Oconnor\\NorthAmerica_SDC\\Navteq_Q32011\\"
-
-Roads = source + "mroads.sdc/mroads"
-arcpy.Clip_analysis(in_features = Roads, clip_features = SA, out_feature_class = "roads.shp")
 
 Streets = source + "streets.sdc\\streets"
 arcpy.Clip_analysis(in_features = Streets, clip_features = SA, out_feature_class = "streets.shp")
