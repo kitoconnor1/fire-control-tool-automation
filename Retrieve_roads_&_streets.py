6bb2fab7a77a207f863ab301018ce9b74633c06c
Quresh Latif <qureshlatif@yahoo.com>
2017-06-07 10:18:47 -0600

    annotation
    Annotation denotes user-specified inputs.

--- Retrieve_roads_&_streets.py
+++ Retrieve_roads_&_streets.py
@@ -6,9 +6,11 @@ from arcpy.sa import *
 arcpy.CheckOutExtension("Spatial")
 import os, sys
 
+######################### Inputs #################################
 arcpy.env.workspace = r"E:\GISData\fire_control_modl_proj" # Set workspace #
 SA = "Tonto_study_area.shp" # Polygon defining study landscape #
 source = "T:\\FS\\RD\\RMRS\\Science\\HD\\MySpace\\Oconnor\\NorthAmerica_SDC\\Navteq_Q32011\\"
+##################################################################
 
 Streets = source + "streets.sdc\\streets"
 arcpy.Clip_analysis(in_features = Streets, clip_features = SA, out_feature_class = "streets.shp")
