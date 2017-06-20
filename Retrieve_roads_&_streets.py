af71dcb696245b572e148226a7c4a02428db00ed
Quresh Latif <qureshlatif@yahoo.com>
2017-06-20 10:01:23 -0600

    Update projection
    Temporarily projects study area shapefile to match NAVTEQ source file for clipping, WGS84, and then projects clipped file back to NAD83.

--- Retrieve_roads_&_streets.py
+++ Retrieve_roads_&_streets.py
@@ -12,5 +12,9 @@ SA = "Tonto_study_area.shp" # Polygon defining study landscape #
 source = "T:\\FS\\RD\\RMRS\\Science\\HD\\MySpace\\Oconnor\\NorthAmerica_SDC\\Navteq_Q32011\\"
 ##################################################################
 
+SA_WGS = arcpy.Project_management(in_dataset = SA, out_dataset = "SA_WGS.shp", out_coor_system="GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
 Streets = source + "streets.sdc\\streets"
-arcpy.Clip_analysis(in_features = Streets, clip_features = SA, out_feature_class = "streets.shp")
+streets_WGS = arcpy.Clip_analysis(in_features = Streets, clip_features = SA_WGS, out_feature_class = "streets_WGS.shp")
+arcpy.Project_management(in_dataset = streets_WGS, out_dataset = "streets.shp", out_coor_system="GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
+arcpy.Delete_management(streets_WGS)
+arcpy.Delete_management(SA_WGS)
